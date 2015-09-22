3a19034675ac28c9bc7d0d519f51c0921740e950
Jia Wang <wj2016@gmail.com>
2015-09-22 21:33:26 +0200

    [Demo Social] empty application template

---
+++ DemoSocial/DemoSocial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoSocial
+//
+//  Created by Jia Wang on 22/09/15.
+//  Copyright (c) 2015 Jia Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
