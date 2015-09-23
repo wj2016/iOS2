d38997967525d6fb0dd5aee5b16507bf8a0e2886
Jia Wang <wj2016@gmail.com>
2015-09-23 20:47:33 +0200

    [Demo iOS Network Programming] empty app template

---
+++ DemoNetwork/DemoNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoNetwork
+//
+//  Created by Jia Wang on 23/09/15.
+//  Copyright (c) 2015 Jia Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoNetwork/DemoNetworkTests/DemoNetworkTests.m
@@ -0,0 +1,40 @@
+//
+//  DemoNetworkTests.m
+//  DemoNetworkTests
+//
+//  Created by Jia Wang on 23/09/15.
+//  Copyright (c) 2015 Jia Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DemoNetworkTests : XCTestCase
+
+@end
+
+@implementation DemoNetworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
